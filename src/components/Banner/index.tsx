174b99b5f65f5761a7324e268c2e13e609e66f66
fabiosdsilva <fabiosds17@gmail.com>
2022-05-06 12:06:43 -0300

    Component banner to desktop and smatphone finish

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,32 @@
+import * as S from './styles'
+
+import Button from '../Button'
+
+export type BannerProps = {
+  image: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Banner = ({
+  image,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink
+}: BannerProps) => (
+  <S.Wrapper>
+    <S.Image src={image} role="img" aria-label={title} />
+    <S.Caption>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+      <Button as="a" href={buttonLink} size="large">
+        {buttonLabel}
+      </Button>
+    </S.Caption>
+  </S.Wrapper>
+)
+
+export default Banner
