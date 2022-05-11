f8b9c9e2beb7b2b8eab378001f73fe1733929e99
fabiosdsilva <fabiosds17@gmail.com>
2022-05-11 16:34:34 -0300

    Add tests to sections of home

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -9,4 +9,19 @@ describe('<Home />', () => {
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
   })
+
+  it('verificar se os headings das sections estão sendo renderizados', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcoming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+  })
 })
