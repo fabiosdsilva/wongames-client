9c692d78cbeed32c73dc4d073c501088ec015b03
fabiosdsilva <fabiosds17@gmail.com>
2022-05-11 16:14:01 -0300

    Add compenent Menu on page home

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,15 @@
+import Container from 'components/Container'
+import Menu from 'components/Menu'
+import * as S from './styles'
+
+const Home = () => (
+  <S.Wrapper>
+    <section>
+      <Container>
+        <Menu></Menu>
+      </Container>
+    </section>
+  </S.Wrapper>
+)
+
+export default Home
