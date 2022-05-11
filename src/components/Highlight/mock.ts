e4d238f4b1547d717074c2f121074851df07dd73
fabiosdsilva <fabiosds17@gmail.com>
2022-05-11 17:40:48 -0300

    Add mock of store

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'King Arthur: Knight" s Tale',
+  subtitle: 'A riveting dark fantasy RPG',
+  buttonLabel: 'Buy now',
+  buttonLink:
+    'https://store.steampowered.com/app/1157390/King_Arthur_Knights_Tale/',
+  backgroundImage: '/img/red-dead-img.jpg'
+}
--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,17 +1,12 @@
 import { Story, Meta } from '@storybook/react'
 import Highlight, { HighlightProps } from '.'
 
+import item from './mock'
+
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'King Arthur: Knight" s Tale',
-    subtitle: 'A riveting dark fantasy RPG',
-    buttonLabel: 'Buy now',
-    buttonLink:
-      'https://store.steampowered.com/app/1157390/King_Arthur_Knights_Tale/',
-    backgroundImage: '/img/red-dead-img.jpg'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => (
