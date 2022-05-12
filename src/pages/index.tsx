74fc06e804adde8026d20d686240f45f42ecb40f
fabiosdsilva <fabiosds17@gmail.com>
2022-05-12 08:54:06 -0300

    Add page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Main from 'components/Main'
+import Home, { HomeTemplateProps } from '../templates/Home'
 
-export default function Home() {
-  return <Main />
+import GamesMock from 'components/GameCardSlider/mock'
+import BannerMock from 'components/BannerSlider/mock'
+import HighlightMock from 'components/Highlight/mock'
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      banners: BannerMock,
+      newGames: GamesMock,
+      mostPopularGames: GamesMock,
+      mostPopularHighlight: HighlightMock,
+      upcommingGames: GamesMock,
+      upcommingHighligth: HighlightMock,
+      upcommingMoreGames: GamesMock,
+      freeGames: GamesMock,
+      freeHighligth: HighlightMock
+    }
+  }
 }
