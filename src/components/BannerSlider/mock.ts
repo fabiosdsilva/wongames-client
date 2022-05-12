453a5239e8c1ca985decefaa76c7bcb677c16885
fabiosdsilva <fabiosds17@gmail.com>
2022-05-12 08:56:29 -0300

    Updated props

--- src/components/BannerSlider/mock.ts
+++ src/components/BannerSlider/mock.ts
@@ -2,55 +2,34 @@ export default [
   {
     id: '1',
     title: 'Population Zero',
-    slug: 'population-zero',
+    subtitle: 'population-zero',
     developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    image: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: '235',
-    promotionalPrice: '215'
+    promotionalPrice: '215',
+    buttonLabel: 'Comprar',
+    buttonLink: '#'
   },
   {
     id: '2',
     title: 'Population Zero',
-    slug: 'population-zero',
+    subtitle: 'population-zero',
     developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    image: 'https://source.unsplash.com/user/willianjusten/300x141',
     price: '235',
-    promotionalPrice: '215'
+    promotionalPrice: '215',
+    buttonLabel: 'Comprar',
+    buttonLink: '#'
   },
   {
     id: '3',
     title: 'Population Zero',
-    slug: 'population-zero',
+    subtitle: 'population-zero',
     developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    image: 'https://source.unsplash.com/user/willianjusten/300x142',
     price: '235',
-    promotionalPrice: '215'
-  },
-  {
-    id: '4',
-    title: 'Population Zero',
-    slug: 'population-zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x143',
-    price: '235',
-    promotionalPrice: '215'
-  },
-  {
-    id: '5',
-    title: 'Population Zero',
-    slug: 'population-zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x144',
-    price: '235',
-    promotionalPrice: '215'
-  },
-  {
-    id: '6',
-    title: 'Population Zero',
-    slug: 'population-zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x145',
-    price: '235',
-    promotionalPrice: '215'
+    promotionalPrice: '215',
+    buttonLabel: 'Comprar',
+    buttonLink: '#'
   }
 ]
