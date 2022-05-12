c91ff7381ad466951e2e04f83f6b37f70cb6db26
fabiosdsilva <fabiosds17@gmail.com>
2022-05-12 15:34:41 -0300

    Finish component Checkbox and tests

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react'
+
 import Checkbox from '.'
 
 export default {
@@ -6,4 +7,4 @@ export default {
   component: Checkbox
 } as Meta
 
-export const Default: Story = () => <Checkbox />
+export const Default: Story = (args) => <Checkbox {...args} />
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -3,9 +3,35 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Checkbox from '.'
+import theme from '../../styles/theme'
 
 describe('<Checkbox />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(<Checkbox />)
+  it('renderizar label e input', () => {
+    const { container } = renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" />
+    )
+
+    // checkbox
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    // checar o id do input e labelFor
+    expect(screen.getByText(/check/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('renderizar sem o checkbox', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('renderizar label com a cor preta', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
