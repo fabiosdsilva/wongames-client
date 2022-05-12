6f04a9459f778ad7d9af9e1e2c8fd8503606ff8d
fabiosdsilva <fabiosds17@gmail.com>
2022-05-12 13:24:25 -0300

    Initial commit

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,11 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(<Checkbox />)
+  })
+})
