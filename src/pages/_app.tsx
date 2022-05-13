c88a19d991c159fbea7434acff89652e39dbb157
fabiosdsilva <fabiosds17@gmail.com>
2022-05-13 09:45:41 -0300

    Add favicon on pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/favicon.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
