4f923b7aa2687a4492c4fe54bbc8d58a47e7dd23
fabiosdsilva <fabiosds17@gmail.com>
2022-05-17 08:30:36 -0300

    Registration page created

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
+import Auth from 'templates/Auth'
+
+export default function Singup() {
+  return (
+    <Auth title="Sign-up">
+      <FormSignUp></FormSignUp>
+    </Auth>
+  )
+}
