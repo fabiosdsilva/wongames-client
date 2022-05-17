a323b4ae4741b8077b3a7ab706c3b3df894bb908
fabiosdsilva <fabiosds17@gmail.com>
2022-05-17 09:18:58 -0300

    Finished page auth

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,8 +1,50 @@
+import Link from 'next/link'
+
 import * as S from './styles'
 
-const Auth = () => (
+import Logo from 'components/Logo'
+import Heading from 'components/Heading'
+
+type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
-    <h1>Auth</h1>
+    <S.BannerBlock>
+      <S.BannerContent>
+        <Link href="/">
+          <a>
+            <Logo id="banner" />
+          </a>
+        </Link>
+
+        <div>
+          <Heading size="huge">All your favorite games in one place</Heading>
+
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
+    </S.BannerBlock>
+
+    <S.Content>
+      <S.ContentWrapper>
+        <Link href="/">
+          <a>
+            <Logo color="black" size="large" id="content" />
+          </a>
+        </Link>
+        <Heading color="black" lineLeft lineColor="secondary">
+          {title}
+        </Heading>
+        {children}
+      </S.ContentWrapper>
+    </S.Content>
   </S.Wrapper>
 )
 
