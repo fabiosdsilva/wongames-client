04305edea905255d1da445ad33ffba09f7881412
fabiosdsilva <fabiosds17@gmail.com>
2022-05-17 08:29:42 -0300

    Login page created

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,10 @@
+import FormSignIn from 'components/FormSignIn'
+import Auth from 'templates/Auth'
+
+export default function Singin() {
+  return (
+    <Auth title="Sign in">
+      <FormSignIn></FormSignIn>
+    </Auth>
+  )
+}
