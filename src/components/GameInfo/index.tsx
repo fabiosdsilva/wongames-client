0cd97fb717a5ab8fe91dddf647fbef539dc1101a
fabiosdsilva <fabiosds17@gmail.com>
2022-05-18 22:42:58 -0300

    Finished styling gameinfo

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -10,23 +10,26 @@ import {
   Favorite
 } from '@styled-icons/material-outlined'
 
-const GameInfo = () => (
+export type GameInfoProps = {
+  title: string
+  description: string
+  price: string
+}
+
+const GameInfo = ({ title, description, price }: GameInfoProps) => (
   <S.Wrapper>
-    <Heading color="black" lineBottom lineColor="primary">
-      Borderlands 3
+    <Heading color="black" lineBottom>
+      {title}
     </Heading>
-    <S.Description>
-      Experience the epic space strategy games that redefined the RTS genre.
-      Control your fleet and build an armada across more than 30 single-player
-      missions.
-    </S.Description>
-    <Ribbon color="secondary">$215,00</Ribbon>
+    <S.Description>{description}</S.Description>
+    <Ribbon color="secondary">{`${price}`}</Ribbon>
 
-    <Button icon={<FavoriteBorder />} minimal>
-      Wishlist
-    </Button>
-
-    <Button icon={<AddShoppingCart />}>Add to cart</Button>
+    <S.ButtonWrapper>
+      <Button icon={<AddShoppingCart />}>Add to cart</Button>
+      <Button icon={<FavoriteBorder />} minimal>
+        Wishlist
+      </Button>
+    </S.ButtonWrapper>
   </S.Wrapper>
 )
 
--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,24 @@
 import { Story, Meta } from '@storybook/react'
-import GameInfo from '.'
+import GameInfo, { GameInfoProps } from '.'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  args: {
+    title: 'Borderlands 3',
+    description:
+      'Experience the epic space strategy games that redefined the RTS genre. Control your fleet and build an armada across more than 30 single-player missions.',
+    price: '250,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <GameInfo />
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '100rem', margin: 'auto' }}>
+    <GameInfo {...args} />
+  </div>
+)
