669b6e9eb2c563956843f3bed10721c3d17422d2
fabiosdsilva <fabiosds17@gmail.com>
2022-05-18 22:52:22 -0300

    Tests finished

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -4,8 +4,31 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
 
+const props = {
+  title: 'My title',
+  description: 'My description',
+  price: '250'
+}
+
 describe('<GameInfo />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(<GameInfo />)
+  it('verificar se heading está sendo renderizado', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /my title/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/my description/i)).toBeInTheDocument()
+    expect(screen.getByText(/250/)).toBeInTheDocument()
+  })
+
+  it('verificar se os botões estão sendo renderizados', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
   })
 })
