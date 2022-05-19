89393b9c5b6b8ca345dd5aa76982d566b052f4ec
fabiosdsilva <fabiosds17@gmail.com>
2022-05-19 16:21:36 -0300

    Finalized initial structure and tests

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,9 +1,80 @@
+import { useState } from 'react'
 import * as S from './styles'
+import Slider, { SliderSettings } from '../Slider'
 
-const Gallery = () => (
-  <S.Wrapper>
-    <h1>Gallery</h1>
-  </S.Wrapper>
-)
+import {
+  ArrowBackIos as ArrowLeft,
+  ArrowForwardIos as ArrowRight
+} from '@styled-icons/material-outlined'
+
+type GalleyImagesProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleyImagesProps[]
+}
+
+const settings: SliderSettings = {
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  prevArrow: <ArrowLeft aria-label="next image" />,
+  nextArrow: <ArrowRight aria-label="previous image" />,
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    }
+  ]
+}
+
+const Gallery = ({ items }: GalleryProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <Slider settings={settings}>
+        {items.map((item, index) => (
+          <img
+            role="button"
+            key={`thumb-${index}`}
+            src={item.src}
+            alt={`Thumb - ${item.label}`}
+            onClick={() => {
+              setIsOpen(true)
+            }}
+          />
+        ))}
+      </Slider>
+      <S.Modal
+        isOpen={isOpen}
+        aria-label="modal"
+        aria-hidden={!isOpen}
+      ></S.Modal>
+    </S.Wrapper>
+  )
+}
 
 export default Gallery
--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,11 +1,40 @@
-import { screen } from '@testing-library/react'
+import 'match-media-mock'
 
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from '.'
+import mockItems from './mock'
 
 describe('<Gallery />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(<Gallery />)
+  it('renderizar o heading', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // Verificar se os botões (title) estão sendo renderizados
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', mockItems[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('verificar se o modal está aberto', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // selecionar o modal
+    const modal = screen.getByLabelText('modal')
+
+    // verificando se está aberto
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+
+    // quando clica com o botão para abrir o modal
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
   })
 })
