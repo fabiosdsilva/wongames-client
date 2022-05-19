f9e568aada8e156a9871d53e8f259df6c72b8c51
fabiosdsilva <fabiosds17@gmail.com>
2022-05-19 09:09:44 -0300

    Tests for the icons

--- src/components/GameDatails/test.tsx
+++ src/components/GameDatails/test.tsx
@@ -5,6 +5,7 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import GameDatails, { GameDatailsProps } from '.'
 
 const props: GameDatailsProps = {
+  developer: 'Gearbox Software',
   platforms: ['windows', 'linux', 'mac']
 }
 
@@ -15,5 +16,22 @@ describe('<GameDatails />', () => {
     expect(
       screen.getByRole('heading', { name: /developer/i })
     ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /editor/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Rating/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
+  })
+
+  it('verificar se está renderizando os ícones', () => {
+    renderWithTheme(<GameDatails {...props} />)
+
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
   })
 })
