93fe969a4eef735097115eff2b48bc46f19de3e7
fabiosdsilva <fabiosds17@gmail.com>
2022-05-20 11:18:16 -0300

    Fix errors

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react'
-import { renderWithTheme } from '../../utils/tests/helpers'
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Heading from '.'
 
@@ -11,79 +12,79 @@ describe('<Heading />', () => {
     })
   })
 
-  // it('should render a black heading when color is passed', () => {
-  //   renderWithTheme(<Heading color="black">Won Games</Heading>)
-  //   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-  //     color: '#030517'
-  //   })
-  // })
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color="black">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
 
-  // it('should render a heading with a line to the left side', () => {
-  //   renderWithTheme(<Heading lineLeft>Won Games</Heading>)
-  //   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-  //     'border-left': '0.7rem solid #F231A5'
-  //   })
-  // })
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #F231A5'
+    })
+  })
 
-  // it('should render a heading with a line at the bottom', () => {
-  //   renderWithTheme(<Heading lineBottom>Won Games</Heading>)
-  //   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-  //     'border-bottom',
-  //     '0.5rem solid #F231A5',
-  //     {
-  //       modifier: '::after'
-  //     }
-  //   )
-  // })
+  it('should render a heading with a line at the bottom', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
 
-  // it('should render a heading with a small size', () => {
-  //   renderWithTheme(<Heading size="small">Won Games</Heading>)
-  //   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-  //     'font-size': '1.6rem'
-  //   })
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
 
-  //   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-  //     'width',
-  //     '3rem',
-  //     {
-  //       modifier: '::after'
-  //     }
-  //   )
-  // })
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
 
-  // it('should render a heading with a huge size', () => {
-  //   renderWithTheme(<Heading size="huge">Won Games</Heading>)
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(<Heading size="huge">Won Games</Heading>)
 
-  //   expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-  //     'font-size': '5.2rem'
-  //   })
-  // })
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '5.2rem'
+    })
+  })
 
-  // it('should render a Heading with a primary line color', () => {
-  //   render(
-  //     <Heading lineColor="primary" lineLeft lineBottom>
-  //       Lorem Ipsum
-  //     </Heading>
-  //   )
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
 
-  //   const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
-  //   expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
-  //   expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
-  //     modifier: '::after'
-  //   })
-  // })
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
 
-  // it('should render a Heading with a secondary line color', () => {
-  //   render(
-  //     <Heading lineColor="secondary" lineLeft lineBottom>
-  //       Lorem Ipsum
-  //     </Heading>
-  //   )
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
 
-  //   const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
-  //   expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
-  //   expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
-  //     modifier: '::after'
-  //   })
-  // })
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
+  })
 })
