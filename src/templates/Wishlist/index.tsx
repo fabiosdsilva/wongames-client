cbb979e136143a560fb75e970985780dc7ab71c4
fabiosdsilva <fabiosds17@gmail.com>
2022-05-20 15:39:01 -0300

    Adjust spacing between menu and content

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,6 +1,7 @@
 import Container from 'components/Container'
 import Heading from 'components/Heading'
 import Base from 'templates/Base'
+
 import * as S from './styles'
 
 const Wishlist = () => (
