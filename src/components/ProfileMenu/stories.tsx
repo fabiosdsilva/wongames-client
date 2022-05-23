743e230c880c2f0ba1930be520990514fd4a4ef9
fabiosdsilva <fabiosds17@gmail.com>
2022-05-23 16:29:41 -0300

    Created profile component structure

---
+++ src/components/ProfileMenu/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import ProfileMenu from '.'
+
+export default {
+  title: 'Profile/ProfileMenu',
+  component: ProfileMenu
+} as Meta
+
+export const Default: Story = () => <ProfileMenu />
