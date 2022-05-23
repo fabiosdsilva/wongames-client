54cf8ede2b01dc5a0e3f99a8b081f102b33c89f1
fabiosdsilva <fabiosds17@gmail.com>
2022-05-23 17:11:21 -0300

    Tested and implemented active links

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import ProfileMenu from '.'
@@ -13,5 +14,14 @@ describe('<ProfileMenu />', () => {
     expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+
+  it('verificar se está renderizando o link ativo corretamente', () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
   })
 })
