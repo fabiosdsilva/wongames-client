14054cc830030ae5a9438e439f2ad78df2e2bd77
fabiosdsilva <fabiosds17@gmail.com>
2022-05-24 10:26:25 -0300

    Finished page My Profile

---
+++ src/pages/profile/me.tsx
@@ -0,0 +1,11 @@
+import Profile from 'templates/Profile'
+import FormProfile from 'components/FormProfile'
+
+export default function Me() {
+  return (
+    <Profile>
+      {' '}
+      <FormProfile />{' '}
+    </Profile>
+  )
+}
---
+++ src/templates/Profile/styles.ts
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Main = styled.main`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.medium};
+
+    ${media.greaterThan('medium')`
+      display: grid;
+      grid-template-columns: 32rem 1fr;
+      gap: calc(${theme.grid.gutter} * 2);
+    `}
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    padding: ${theme.spacings.xxsmall};
+    color: ${theme.colors.black};
+
+    ${media.greaterThan('medium')`
+        padding: ${theme.spacings.small};
+    `}
+  `}
+`
